d0a5224ebcae33d509c0d5fbb9527f7d537d4f47
Prashant Arya <prary2@gmail.com>
2019-07-04 08:26:23 +0530

    fixing

--- pkg/skaffold/build/cluster/sources/sources_test.go
+++ pkg/skaffold/build/cluster/sources/sources_test.go
@@ -219,17 +219,17 @@ func TestSetProxy(t *testing.T) {
 			description:    "set http proxy",
 			clusterDetails: &latest.ClusterDetails{HTTPProxy: "proxy.com"},
 			env:            []v1.EnvVar{},
-			expectedArgs:   []v1.EnvVar{{Name: "HTTPProxy", Value: "proxy.com"}},
+			expectedArgs:   []v1.EnvVar{{Name: "HTTP_PROXY", Value: "proxy.com"}},
 		}, {
 			description:    "set https proxy",
 			clusterDetails: &latest.ClusterDetails{HTTPSProxy: "proxy.com"},
 			env:            []v1.EnvVar{},
-			expectedArgs:   []v1.EnvVar{{Name: "HTTPSProxy", Value: "proxy.com"}},
+			expectedArgs:   []v1.EnvVar{{Name: "HTTP_PROXY", Value: "proxy.com"}},
 		}, {
 			description:    "set http and https proxy",
 			clusterDetails: &latest.ClusterDetails{HTTPProxy: "proxy.com", HTTPSProxy: "proxy.com"},
 			env:            []v1.EnvVar{},
-			expectedArgs:   []v1.EnvVar{{Name: "HTTPProxy", Value: "proxy.com"}, {Name: "HTTPSProxy", Value: "proxy.com"}},
+			expectedArgs:   []v1.EnvVar{{Name: "HTTP_PROXY", Value: "proxy.com"}, {Name: "HTTPS_PROXY", Value: "proxy.com"}},
 		},
 	}
 
